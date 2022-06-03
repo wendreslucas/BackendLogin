0f5b4bd2d547f1af38f20b904fd01cc476c658df
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-06-03 10:33:56 -0300

    iniciando o deploy backend

---
+++ src/controllers/UserController.js
@@ -0,0 +1,91 @@
+const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  async index(req, res) {
+    try {
+      const users = await User.find()
+      return res.status(200).json({ users })
+    } catch (error) {
+      return res.status(400).json({ error: error.message })
+    }
+  },
+
+  async store(req, res) {
+    const { name, email, password, confirmPassword } = req.body
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ error: 'Preencha todos os campos' })
+    }
+    // if (password !== confirmPassword) {
+    //   return res.status(400).json({ error: 'As senhas não conferem' })
+    // }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ error: 'A senha deve ter no mínimo 6 caracteres' })
+    }
+
+    const emailExist = await User.findOne({ email: email })
+    if (emailExist) {
+      return res.status(400).json({ error: 'Esse Email já existe' })
+    }
+
+    const nameExist = await User.findOne({ name: name })
+    if (nameExist) {
+      return res.status(404).json({ error: 'Esse Username já existe' })
+    }
+
+    // const salt = bcrypt.genSaltSync(10)
+    // const passwordHash = bcrypt.hashSync(password, salt)
+
+    const user = new User({
+      name,
+      email,
+      password
+    })
+
+    try {
+      await user.save()
+      return res.status(201).json({ user })
+    } catch (error) {
+      return res.status(500).json({ error: error.message })
+    }
+  },
+
+  async login(req, res) {
+    const { name, password } = req.body
+
+    if (!name || !password) {
+      return res.status(400).json({ error: 'Preencha todos os campos' })
+    }
+
+    const user = await User.findOne({ name: name })
+    if (!user) {
+      return res.status(404).json({ error: 'Usuário não encontrado' })
+    }
+
+    // const checkPassword = bcrypt.compareSync(password, user.password)
+    // if (!checkPassword) {
+    //   return res.status(401).json({ error: 'Senha incorreta' })
+    // }
+    // return res.status(200).json({ MSG: 'Deu certo!' })
+
+    const userPassword = await User.findOne({ password })
+
+    if (!userPassword) {
+      return res.status(422).json({ error: 'Senha incorreta' })
+    }
+
+    try {
+      const secret = process.env.SECRET
+
+      const token = jwt.sign({ id: user._id }, secret)
+      return res.status(200).json({ MSG: 'Autenticado com sucesso', token })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).json({ error: error.message })
+    }
+  }
+}
