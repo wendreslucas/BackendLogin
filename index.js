8719eabd8db6ac0f452cf025407a7e41c538f3fa
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-06-03 11:05:47 -0300

    ajustes indexJS

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
 require('dotenv').config()
 const express = require('express')
+const app = express()
+PORT = process.env.PORT || 4000
+
+const routes = require('./src/routes/routes')
+const connectToDataBase = require('./src/database')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const connectToDataBase = require('./src/database')
-const routes = require('./src/routes/routes')
-port = process.env.PORT || 4000
 
 connectToDataBase()
-const app = express()
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(bodyParser.json())
 app.use(routes)
 
-app.get('/', (req, res) => {
-  res.status(200).json({ MSG: 'Bem vindoS!' })
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`)
+app.get('/', (req, res) => {
+  res.status(200).json({ MSG: 'Bem vindoS!' })
 })
